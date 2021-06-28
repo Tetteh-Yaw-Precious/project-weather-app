92c401ee36abfef7975ee14a5892d9bf0d0c6d00
Tetteh Yaw Precious <precioustetteh38@gmail.com>
2021-06-28 08:00:04 +0800

    Initial commit

---
+++ weatherapp.js
@@ -0,0 +1,21 @@
+const key = 'iNpwVGkhmGd7yvYwbAMGdhoi0pkREA7w';
+
+//get weather information
+const getWeather = async (locationId) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${locationId}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+//get city information
+const getCity = async (city) =>{
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base+query);
+    const data = await response.json();
+    return data[0];
+};
